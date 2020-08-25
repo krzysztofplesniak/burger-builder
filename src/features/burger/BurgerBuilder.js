35cebf30f2e22cf6a7b70e674101d30f961458db
krzysztof plesniak <krzysztofplesniak@interia.pl>
2020-08-25 09:39:05 +0200

    Added UI button for Meat, Chease, Bacon

--- src/features/burger/BurgerBuilder.js
+++ src/features/burger/BurgerBuilder.js
@@ -1,74 +1,84 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-    ingridiensAmount,
-    ingridiensPrice,
-    totalPrice,
-    calculatePrice,
-    orderBurger,
-    moreMeat,
-    lessMeat,
-    moreBacon,
-	lessdBacon,
+	ingridiensAmount,
+	ingridiensPrice,
+	totalPrice,
+	calculatePrice,
+	orderBurger,
+	moreMeat,
+	lessMeat,
+	moreBacon,
+	lessBacon,
+	moreChease,
+	lessChease,
 	moreOnion,
-	lessdOnion,
+	lessOnion,
 	moreCocummber,
 	lessCocummber,
-	moreCheasem,
-	lessChease,
 	moreSalat,
 	lessSalat,
 } from './burgerSlice';
 import styles from './Burger.module.css';
 
 export function BurgerBuilder() {
-  const ingridiens = useSelector(ingridiensAmount);
-  const price = useSelector(ingridiensPrice);
-  const total =  useSelector(totalPrice);
+	const ingridiens = useSelector(ingridiensAmount);
+	const price = useSelector(ingridiensPrice);
+	const total = useSelector(totalPrice);
 
-  const dispatch = useDispatch();
-  
-  return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Less"
-          onClick={() => dispatch(moreMeat())}
-        >
-          +
-        </button>
-        <span className={styles.value}>Burger amount</span>
-        <button
-          className={styles.button}
-          aria-label="More"
-          onClick={() => dispatch(lessMeat())}
-        >
-          -
-        </button>
-      </div>
-      {/* <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() =>
-            dispatch(incrementByAmount(Number(incrementAmount) || 0))
-          }
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
-          Add Async
-        </button>
-      </div> */}
-    </div>
-  );
+	const dispatch = useDispatch();
+
+	return (
+		<>
+			<div className={styles.row}>
+				{/* MEAT */}
+				<button
+					className={styles.button}
+					aria-label='Less'
+					onClick={() => dispatch(moreMeat())}>
+					+
+				</button>
+				<span className={styles.value}>Meat</span>
+				<button
+					className={styles.button}
+					aria-label='More'
+					onClick={() => dispatch(lessMeat())}>
+					-
+				</button>
+			</div>
+			{/* CHEASE */}
+			<div className={styles.row}>
+				<button
+					className={styles.button}
+					aria-label='Less'
+					onClick={() => dispatch(moreChease())}>
+					+
+				</button>
+				<span className={styles.value}>Chease</span>
+				<button
+					className={styles.button}
+					aria-label='More'
+					onClick={() => dispatch(lessChease())}>
+					-
+				</button>
+			</div>
+			<div className={styles.row}>
+				{/* BEACON */}
+
+				<button
+					className={styles.button}
+					aria-label='Less'
+					onClick={() => dispatch(moreBacon())}>
+					+
+				</button>
+				<span className={styles.value}>Chease</span>
+				<button
+					className={styles.button}
+					aria-label='More'
+					onClick={() => dispatch(lessBacon())}>
+					-
+				</button>
+			</div>
+		</>
+	);
 }
